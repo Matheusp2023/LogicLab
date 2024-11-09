c874da3a972a9f8fba76178c5648a3afb21fefb4
Matheus Pedro <mps@ic.ufal.br>
2024-11-08 23:35:18 -0300

    Implementação do support para o projeto

--- LogicLabProject/src/org/ufal/logiclabproject/project/LogicLabProject.java
+++ LogicLabProject/src/org/ufal/logiclabproject/project/LogicLabProject.java
@@ -4,32 +4,56 @@
  */
 package org.ufal.logiclabproject.project;
 
+import java.io.IOException;
 import org.netbeans.api.project.Project;
 import org.netbeans.spi.project.ProjectState;
 import org.openide.filesystems.FileObject;
+import org.openide.util.Exceptions;
 import org.openide.util.Lookup;
+import org.openide.util.lookup.Lookups;
 
 /**
  *
  * @author Matheus Pedro (mps@ic.ufal.br)
  */
 public class LogicLabProject implements Project {
+    
+    public static final String CIRCUITS_DIR = "circuits";
+    
+    private final FileObject projectDir;
+    private final ProjectState state;
+    private Lookup lookup;
 
-    public LogicLabProject(FileObject projectDirectory, ProjectState state) {
+    public LogicLabProject(FileObject projectDir, ProjectState state) {
+        this.projectDir = projectDir;
+        this.state = state;
     }
 
     @Override
     public FileObject getProjectDirectory() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return projectDir;
     }
 
     @Override
     public Lookup getLookup() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        if (lookup == null) {
+            lookup = Lookups.fixed(new Object[] {
+            this,
+            state});
+        }
+        return lookup;
     }
 
-    void getCircuitsFolder(boolean b) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public FileObject getCircuitsFolder(boolean create) {
+        FileObject result = projectDir.getFileObject(CIRCUITS_DIR);
+        if (result == null && create) {
+            try {
+                result = projectDir.createFolder(CIRCUITS_DIR);
+            } catch (IOException ex) {
+                Exceptions.printStackTrace(ex);
+            }
+        }
+        return result;
     }
     
 }
